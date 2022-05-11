c30ed3f77c04d26467db8c5a52951f3e424c4b60
Jamil Omar <jfalconi63@hotmail.com>
2022-05-11 12:06:54 -0400

    feat: build site

---
+++ build/precache-manifest.6c9daaa619fb05bf4989818530510c44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96977cf27870f9c27560903d00bb8770",
+    "url": "/index.html"
+  },
+  {
+    "revision": "874c306107daadc77973",
+    "url": "/static/css/main.7a4f8f29.chunk.css"
+  },
+  {
+    "revision": "862d2117c26d187189a2",
+    "url": "/static/js/2.b194cd24.chunk.js"
+  },
+  {
+    "revision": "874c306107daadc77973",
+    "url": "/static/js/main.36c32cb1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
